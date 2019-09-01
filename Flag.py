23dfc4124d32ffa03dd1d7fe96606a889297cfdc
Roman M. Gerasimov <48310403+GerasimovRM@users.noreply.github.com>
2019-09-01 19:55:47 +0500

    Add draw of blue color of flag (Version 2)

--- Flag.py
+++ Flag.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((300, 500))
 
 pygame.draw.rect(screen, pygame.Color("#D2691E"), [(30, 20), (10, 450)])
 pygame.draw.rect(screen, pygame.Color("#D2691E"), [(20, 470), (30, 10)])
-pygame.draw.rect(screen, pygame.Color("white"), [(40, 20), (250, 50)])
-pygame.draw.rect(screen, pygame.Color("blue"), [(40, 70), (250, 50)])
+# with error
+pygame.draw.rect(screen, pygame.Color("blue"), [(40, 70), (250, 250)])
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
