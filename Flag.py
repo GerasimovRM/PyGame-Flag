f15334aa6f930e722103bbda7f4df10f1e449392
Roman M. Gerasimov <48310403+GerasimovRM@users.noreply.github.com>
2019-09-01 19:46:10 +0500

    Add draw of flagpole base

--- Flag.py
+++ Flag.py
@@ -5,6 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((300, 500))
 
 pygame.draw.rect(screen, pygame.Color("#D2691E"), [(30, 20), (10, 450)])
+pygame.draw.rect(screen, pygame.Color("#D2691E"), [(20, 470), (30, 10)])
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
